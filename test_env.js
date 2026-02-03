4cc46a38439137d6bc390563515fe18476b4c8d9
DeadZone-0 <riyalkrypton@outlook.com>
2026-02-03 21:29:54 +0530

    Delete test_env.js

--- test_env.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config();
-console.log('DISCORD_TOKEN:', process.env.DISCORD_TOKEN ? `FOUND (${process.env.DISCORD_TOKEN.substring(0, 5)}...)` : 'MISSING');
-console.log('Kai:', process.env.Kai ? 'FOUND' : 'MISSING');
-console.log('Current Dir:', process.cwd());
-console.log('Env file path:', require('path').resolve('.env'));
